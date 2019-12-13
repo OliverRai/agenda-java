5a0490b7d4b16fba2d398dc48990cab7da111170
OliverRai <raiane.mateus.oliveira@gmail.com>
2019-12-13 16:19:55 -0300

    error page access

--- src/br/com/caelum/dao/ConnectionFactory.java
+++ src/br/com/caelum/dao/ConnectionFactory.java
@@ -10,7 +10,7 @@ public class ConnectionFactory {
 	    try {
 	            //Class.forName("com.mysql.jdbc.Driver");
 	            DriverManager.registerDriver(new com.mysql.jdbc.Driver()); //Essa linha foi a diferença
-	            return DriverManager.getConnection("jdbc:mysql://localhost/fj21?useTimezone=true&serverTimezone=America/Sao_Paulo", "toor", "laberinto1");
+	            return DriverManager.getConnection("jdbc:mysql://localhost/fj21?useTimezone=true&serverTimezone=America/Sao_Paulo", "root", "laberinto1");
 	            } catch(SQLException e) {
 	                e.printStackTrace();
 	                throw new RuntimeException(e);
