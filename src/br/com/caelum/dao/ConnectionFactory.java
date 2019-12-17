48b7488e40673d25c5763894bd46072f513d0fd5
OliverRai <raiane.mateus.oliveira@gmail.com>
2019-12-17 19:45:18 -0300

    date calendar

--- src/br/com/caelum/dao/ConnectionFactory.java
+++ src/br/com/caelum/dao/ConnectionFactory.java
@@ -9,12 +9,12 @@ public class ConnectionFactory {
 	    System.out.println("Conectando ao banco");
 	    try {
 	            //Class.forName("com.mysql.jdbc.Driver");
-	            DriverManager.registerDriver(new com.mysql.jdbc.Driver()); //Essa linha foi a diferença
+	            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver()); //Essa linha foi a diferença
 	            return DriverManager.getConnection("jdbc:mysql://localhost/fj21?useTimezone=true&serverTimezone=America/Sao_Paulo", "root", "laberinto1");
 	            } catch(SQLException e) {
 	                e.printStackTrace();
 	                throw new RuntimeException(e);
-	             
+	                
 	        }
 	         
 	    }
--- src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -18,40 +18,32 @@ import br.com.caelum.modelo.Contato;
 
 @WebServlet("/adicionaContato")
 public class AdicionaContatoServlet extends HttpServlet {
-	protected void service(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
-	// busca o writer
-		
-	PrintWriter out = response.getWriter();
-	// buscando os parâmetros no request
-	String nome = request.getParameter("nome");
-	String endereco = request.getParameter("endereco");
-	String email = request.getParameter("email");
-	String dataEmTexto = request.getParameter("dataNascimento");
-	Calendar dataNascimento = null;
-	// fazendo a conversão da data
-	try {
-	Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
-	dataNascimento = Calendar.getInstance();
-	dataNascimento.setTime(date);
-	} catch (ParseException e) {
-	out.println("Erro de conversão da data");
-	return; //para a execução do método
-	}
-	// monta um objeto contato
-	Contato contato = new Contato();
-	contato.setNome(nome);
-	
-	contato.setEndereco(endereco);
-	contato.setEmail(email);
-	contato.setDataNascimento(dataNascimento);
-	// salva o contato
-	ContatoDao dao = new ContatoDao();
-	dao.adiciona(contato);
-	// imprime o nome do contato que foi adicionado
-	out.println("<html>");
-	out.println("<body>");
-	out.println("Contato " + contato.getNome() + " adicionado com sucesso");
-	out.println("</body>");
-	out.println("</html>");
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// busca o writer
+
+		PrintWriter out = response.getWriter();
+		// buscando os parâmetros no request
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String email = request.getParameter("email");
+		Calendar dataNascimento = Calendar.getInstance();
+
+		// monta um objeto contato
+		Contato contato = new Contato();
+		contato.setNome(nome);
+
+		contato.setEndereco(endereco);
+		contato.setEmail(email);
+		contato.setDataNascimento(dataNascimento);
+		// salva o contato
+		ContatoDao dao = new ContatoDao();
+		dao.adiciona(contato);
+		// imprime o nome do contato que foi adicionado
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contato " + contato.getNome() + " adicionado com sucesso");
+		out.println("</body>");
+		out.println("</html>");
 	}
 }
